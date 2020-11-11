3cde9d6f8828892d5371b863e204314d4e1bb43c
junidev <junigomez1305@gmail.com>
2020-11-11 11:28:47 -0500

    cap 5
    cap 5

---
+++ producer_consumer.c
@@ -0,0 +1,44 @@
+#include <omp.h>
+#include <stdio.h>
+
+#define N        10000
+
+#define SEED       2531
+#define RAND_MULT  1366
+#define RAND_ADD   150889
+#define RAND_MOD   714025
+int randy = SEED;
+
+void fill_rand(int length, double *a)
+{
+   int i;
+   for (i=0;i<length;i++) {
+     randy = (RAND_MULT * randy + RAND_ADD) % RAND_MOD;
+     *(a+i) = ((double) randy)/((double) RAND_MOD);
+   }
+}
+
+double Sum_array(int length, double *a)
+{
+   int i;  double sum = 0.0;
+   for (i=0;i<length;i++)  sum += *(a+i);
+   return sum;
+}
+
+int main()
+{
+  double *A, sum, runtime;
+  int flag = 0;
+
+  A = (double *)malloc(N*sizeof(double));
+
+  runtime = omp_get_wtime();
+
+  fill_rand(N, A);  // Producer
+
+  sum = Sum_array(N, A);  // Consumer
+
+  runtime = omp_get_wtime() - runtime;
+
+  printf(" In %f seconds, The sum is %f \n",runtime,sum);
+}
